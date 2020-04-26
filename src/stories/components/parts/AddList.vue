b6ef423495931d99662bac743406f0e276e25782
Nozomi Hosaka <hosaka.non.work@gmail.com>
2020-04-26 10:56:05 +0900

    リストの末尾に新規にリストを追加できるコンポーネントを作成

---
+++ src/stories/components/parts/AddList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <list
+      :lists="lists"
+      footer
+    >
+      <template #default="list">
+        {{ list.content.name }}
+      </template>
+      <template #footer>
+        <slot name="footer">
+          <div
+            class="text-center link"
+            @click="clickAddList"
+          >
+            リストを追加
+          </div>
+        </slot>
+      </template>
+    </list>
+  </div>
+</template>
+
+<script>
+import List from './List';
+
+export default {
+  name: 'AddList',
+  components: {List},
+  props: {
+    lists: {
+      type: Array,
+      required: true,
+    }
+  },
+  methods: {
+    clickAddList() {
+      this.$emit('add');
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/stories/pages/ListPage.vue
+++ src/stories/pages/ListPage.vue
@@ -1,19 +1,75 @@
 <template>
   <div>
-    <list :lists="lists">
-      <template #default="list">
-        {{ list.content.name }}
-      </template>
-    </list>
+    <div class="m-1">
+      <list :lists="lists">
+        <template #default="list">
+          {{ list.content.name }}
+        </template>
+      </list>
+    </div>
+
+    <div class="m-1">
+      <list
+        :lists="lists"
+        header
+      >
+        <template #header>
+          ヘッダー
+        </template>
+        <template #default="list">
+          {{ list.content.name }}
+        </template>
+      </list>
+    </div>
+
+    <div class="m-1">
+      <list
+        :lists="lists"
+        footer
+      >
+        <template #default="list">
+          {{ list.content.name }}
+        </template>
+        <template #footer>
+          フッター
+        </template>
+      </list>
+    </div>
+
+    <div class="m-1">
+      <list
+        :lists="lists"
+        header
+        footer
+      >
+        <template #header>
+          ヘッダー
+        </template>
+        <template #default="list">
+          {{ list.content.name }}
+        </template>
+        <template #footer>
+          フッター
+        </template>
+      </list>
+    </div>
+
+    <div class="m-1">
+      <add-list
+        :lists="listsForAdd"
+        @add="addList"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import List from '../components/parts/List';
+import AddList from '../components/parts/AddList';
 
 export default {
   name: 'ListPage',
-  components: {List},
+  components: {AddList, List},
   data() {
     return {
       lists: [
@@ -23,12 +79,26 @@ export default {
         {
           'name': 'test02'
         },
+      ],
+      listsForAdd: [
+        {
+          'name': 'test01'
+        },
+        {
+          'name': 'test02'
+        },
       ]
+
     };
+  },
+  methods: {
+    addList() {
+      this.listsForAdd.push({'name': 'add' + Number(this.listsForAdd.length + 1)});
+    }
   }
 };
 </script>
 
 <style scoped>
-
+    @import "../sass/app.scss";
 </style>
