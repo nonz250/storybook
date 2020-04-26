a5b4a5792b6686693d3123ac5cbea50f9680ec75
Nozomi Hosaka <hosaka.non.work@gmail.com>
2020-04-26 10:55:36 +0900

    リストの仕様を変更

--- src/stories/components/parts/List.vue
+++ src/stories/components/parts/List.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    <div class="list">
-      <div
-        v-for="(list, index) in lists"
-        :key="index"
-        class="list-item"
-      >
-        <slot :content="list" />
-      </div>
+  <div class="list">
+    <div
+      v-if="header"
+      class="list-item"
+    >
+      <slot name="header" />
+    </div>
+    <div
+      v-for="(list, index) in lists"
+      :key="index"
+      class="list-item"
+    >
+      <slot :content="list" />
+    </div>
+    <div
+      v-if="footer"
+      class="list-item"
+    >
+      <slot name="footer" />
     </div>
   </div>
 </template>
@@ -16,6 +26,14 @@
 export default {
   name: 'List',
   props: {
+    header: {
+      type: Boolean,
+      default: false,
+    },
+    footer: {
+      type: Boolean,
+      default: false,
+    },
     lists: {
       type: Array,
       required: true,
@@ -25,17 +43,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.list {
-  list-style: none;
-  margin: 0;
-  border: black solid 1px;
-  border-radius: 5px;
-  .list-item {
-    padding: 0.5rem;
-    border-bottom: black solid 1px;
-    &:last-child {
-      border-bottom: none;
+    .list {
+        list-style: none;
+        margin: 0;
+        border: black solid 1px;
+        border-radius: 5px;
+
+        .list-item {
+            padding: 0.5rem;
+            border-bottom: black solid 1px;
+
+            &:last-child {
+                border-bottom: none;
+            }
+        }
     }
-  }
-}
 </style>
