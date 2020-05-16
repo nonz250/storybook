10ad434b70c9f9d24d975788a70d9aba20741a08
Nozomi Hosaka <hosaka.non.work@gmail.com>
2020-05-17 07:43:32 +0900

    ナビゲーターのアクション部分を作成

--- src/stories/components/parts/Navigator.vue
+++ src/stories/components/parts/Navigator.vue
@@ -7,12 +7,18 @@
     >
       <i class="fas fa-bars" />
     </div>
+    <div class="navigator__action">
+      <atom-button>action1</atom-button>
+      <atom-button>action2</atom-button>
+    </div>
   </nav>
 </template>
 
 <script>
+import AtomButton from '../atoms/AtomButton';
 export default {
   name: 'Navigator',
+  components: {AtomButton},
   props: {
     value: {
       type: Boolean,
