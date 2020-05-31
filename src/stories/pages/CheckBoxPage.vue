f5ad82f0fd118aba7671d5454c1b4e40bf160bf7
Nozomi Hosaka <hosaka.non.work@gmail.com>
2020-05-31 11:17:11 +0900

    チェックボックスを作成

---
+++ src/stories/pages/CheckBoxPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col">
+        <atom-check-box v-model="check1" />check1: {{ check1 }}
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="button-group">
+          <atom-check-box v-model="check2" />check2: {{ check2 }}
+          <atom-check-box v-model="check3" />check3: {{ check3 }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AtomCheckBox from '../components/atoms/AtomCheckBox';
+
+export default {
+  name: 'CheckBoxPage',
+  components: {AtomCheckBox},
+  data() {
+    return {
+      check1: false,
+      check2: false,
+      check3: false,
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+    @import "../sass/app.scss";
+</style>
