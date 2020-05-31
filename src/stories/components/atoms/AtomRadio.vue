26f982e2c500546273d192b4c3c34ce82434dc38
suguki <suguki.cr@gmail.com>
2020-05-31 11:16:08 +0900

    wip: 作成中

--- src/stories/components/atoms/AtomRadio.vue
+++ src/stories/components/atoms/AtomRadio.vue
@@ -1,16 +1,41 @@
 <template>
   <div>
-    <input
-      type="radio"
-      class="radio"
-    ><atom-button>ラジオボタン</atom-button>
+    <label style="cursor: pointer;">
+      <input
+        v-model="radio"
+        type="radio"
+        class="radio"
+      ><atom-button
+        :outline="!radio"
+        style="pointer-events: none;"
+        @click="radio = true"
+      >
+        ラジオボタン
+      </atom-button>
+    </label>
   </div>
 </template>
 <script>
 import AtomButton from './AtomButton';
 export default {
   name: 'AtomRadio',
-  components: {AtomButton}
+  components: {AtomButton},
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    }
+  },
+  computed: {
+    radio: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      }
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
