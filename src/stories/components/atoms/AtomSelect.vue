f522e3880e872c7b69e6e565b42e8c8f2a862d67
suguki <suguki.cr@gmail.com>
2020-06-09 21:56:45 +0900

    wip: 作成中

---
+++ src/stories/components/atoms/AtomSelect.vue
@@ -0,0 +1,16 @@
+<template>
+  <select>
+    <option>a</option>
+    <option>b</option>
+    <option>c</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'AtomSelect',
+};
+</script>
+
+<style>
+</style>
